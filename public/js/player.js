5634b3099d7cc0bb56bcb0fd6de117922739f34f
Ryan Kois <ryan.kois@gmail.com>
2014-10-07 20:09:38 -0700

    Update player, make it do stuff.

--- public/js/player.js
+++ public/js/player.js
@@ -3,11 +3,26 @@ var sounds = require('./sounds')
 module.exports = function() {
   var soundList = document.getElementById('sounds');
 
-  sounds.forEach(function(sound) {
+  var add = function(sound) {
     var newSound = document.createElement('li');
-    var soundTitle = document.createElement('span');
-    soundTitle.textContent = sound.title;
-    newSound.appendChild(soundTitle);
+    newSound.innerHTML = '' +
+      '<span>' + sound.title + '</span>' +
+      '<audio src="/sounds/' + sound.file + '"></audio>';
     soundList.appendChild(newSound);
-  });
+  }
+
+  var play = function(ev) {
+    var sound;
+    if (ev.target.nodeName === 'LI') {
+      sound = ev.target;
+    }
+    else if (ev.target.nodeName === 'SPAN') {
+      sound = ev.target.parentNode;
+    }
+    var clip = sound.querySelector('audio');
+    clip.play();
+  }
+
+  sounds.forEach(add);
+  soundList.addEventListener('click', play);
 }
